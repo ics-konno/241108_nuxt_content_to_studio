01c6ad2d3bb673fa440d8548c8828b0a6d03bde7
ics-konno <konno@ics-web.jp>
2024-11-13 21:40:49 +0900

    記法の変更

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts"></script>
+
 <template>
-  <ContentNavigation v-slot="{ navigation = [] }">
-    <ul>
-      <li v-for="link of navigation" :key="link._path">
-        <NuxtLink :to="link._path" class="link">{{ link.title }}</NuxtLink>
-      </li>
-    </ul>
+  <ContentNavigation>
+    <template #default="{ navigation }">
+      <ul>
+        <li v-for="link of navigation" :key="link._path">
+          <NuxtLink :to="link._path" class="link">{{ link.title }}</NuxtLink>
+        </li>
+      </ul>
+    </template>
+    <template #empty />
   </ContentNavigation>
 </template>
 
