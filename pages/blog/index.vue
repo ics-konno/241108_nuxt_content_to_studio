00a635a38ca73aff802f8b9293027ac10fb18f37
ics-konno <konno@ics-web.jp>
2024-11-13 23:07:31 +0900

    絞り込みの修正

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
-const query = queryContent("blog").sort([{ date: -1 }]);
-const { data } = await useAsyncData("blog", () => query.find());
+import { parseDate } from "~/utils/parseDate";
+import type { QueryBuilderParams } from "@nuxt/content";
+
+const query: QueryBuilderParams = {
+  sort: { date: -1 },
+};
 </script>
 
 <template>
   <main>
-    <ArticleList :list="data" />
+    <TagLinks />
+    <ContentList v-slot="{ list }" :query="query" path="/blog">
+      <ul class="list">
+        <li v-for="article in list" :key="article._path">
+          <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+          <span class="date">{{ parseDate(article.date) }}</span>
+        </li>
+      </ul>
+    </ContentList>
   </main>
 </template>
-
-<style scoped></style>
+<style scoped>
+.list {
+  display: grid;
+  gap: 0.5rem;
+}
+.date {
+  font-size: 0.8rem;
+  color: #555;
+  margin-left: 0.75rem;
+}
+</style>
--- pages/blog/tag/[tag].vue
+++ pages/blog/tag/[tag].vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
+import { parseDate } from "~/utils/parseDate";
 import type { QueryBuilderParams } from "@nuxt/content";
+
 const route = useRoute();
-const query: QueryBuilderParams = queryContent("blog")
-  .where({
+const query: QueryBuilderParams = {
+  where: {
     tag: {
       $contains: route.params.tag,
     },
-  })
-  .sort([{ date: -1 }]);
-const { data } = await useAsyncData("tag", () => query.find());
+  },
+  sort: { date: -1 },
+};
 </script>
 
 <template>
   <main>
-    <ArticleList :list="data" />
+    <TagLinks />
+    <ContentList v-slot="{ list }" :query="query" path="/blog">
+      <ul class="list">
+        <li v-for="article in list" :key="article._path">
+          <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+          <span class="date">{{ parseDate(article.date) }}</span>
+        </li>
+      </ul>
+    </ContentList>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.list {
+  display: grid;
+  gap: 0.5rem;
+}
+.date {
+  font-size: 0.8rem;
+  color: #555;
+  margin-left: 0.75rem;
+}
+</style>
