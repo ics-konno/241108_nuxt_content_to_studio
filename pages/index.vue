50bc13a54db0a45ed1cec6d843d732a07a63ae98
ics-konno <konno@ics-web.jp>
2025-02-04 10:47:16 +0900

    preview apiの追加

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,9 @@ const { data } = await useAsyncData("index", () =>
 const { data: all } = await useAsyncData("all", () =>
   queryCollection("docs").all(),
 );
-const { data: naivgation } = await useAsyncData("navigation", () =>
-  queryCollectionNavigation("blog"),
-);
 </script>
 
 <template>
-  <div>{{ JSON.stringify(all) }}</div>
-  <div>{{ JSON.stringify(naivgation) }}</div>
   <ContentRenderer v-if="data" :value="data" />
   <div v-else>
     <h1>Page not found</h1>
